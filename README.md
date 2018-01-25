9fb3870c00431c5a84e36056a0d05bd3687ef16e
picorana <dibartolomeo.sara@gmail.com>
2018-01-25 11:06:41 +0100

    fixed tests, updated readme

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# mONET
+<h1 style="text-align:center">m<b>onet<b></h1>
 
 [![Build Status](https://travis-ci.org/picorana/mONET.svg?branch=master)](https://travis-ci.org/picorana/mONET)   [![codecov](https://codecov.io/gh/picorana/mONET/branch/master/graph/badge.svg)](https://codecov.io/gh/picorana/mONET)
+[![Requirements Status](https://requires.io/github/picorana/mONET/requirements.svg?branch=master)](https://requires.io/github/picorana/mONET/requirements/?branch=master)
 
-ONtology ExTraction from question/answer systems
+<p style="text-align:center; color:gray">
+<b>m</b> stands for nothing <br>
+<b>on</b> stands for <b>on</b>tology <br>
+<b>et</b> stands for <b>e</b>x<b>t</b>raction
+</p>
+
+ontology extraction from question/answer systems
--- monet/tests/test.py
+++ monet/tests/test.py
@@ -4,41 +4,31 @@ from nose.tools import (assert_equal, assert_not_equal,
 
 from monet.classes import *
 
-def run():
-    try: import nose
-    except ImportError:
-        raise ImportError("nose package required to run tests")
+print("Running tests:")
 
-    print("Running tests:")
+try: import nose
+except ImportError:
+    raise ImportError("nose package required to run tests")
 
-    nose.run()
+def test1():
+    assert_true(1==1)
 
-class Test():
+def test_qa_set_question_init():
+    qa = QA_set("hello", "goodbye")
+    assert_equal(str(qa.question), "hello")
+    assert_equal(str(qa.answer), "goodbye")
 
-    print("Running tests:")
+def test_question_detect_type():
+    question = Question("")
+    assert_raises(ValueError, question.detect_type)
 
-    def test1(self):
-        assert_true(1==1)
+    question = Question("Hello")
+    assert_raises(ValueError, question.detect_type)
 
-    def test_qa_set_question_init(self):
-        qa = QA_set("hello", "goodbye")
-        assert_equal(str(qa.question), "hello")
-        assert_equal(str(qa.answer), "goodbye")
+    question = Question("Does it work")
+    question.detect_type()
+    assert_equal(question.type, "closed-ended")
 
-    def test_question_detect_type(self):
-        question = Question("")
-        assert_raises(ValueError, question.detect_type)
-
-        question = Question("Hello")
-        assert_raises(ValueError, question.detect_type)
-
-        question = Question("Does it work")
-        question.detect_type()
-        assert_equal(question.type, "closed-ended")
-
-        question = Question("Is he married or not")
-        question.detect_type()
-        assert_equal(question.type, "open-ended")
-
-if __name__ == "__main__":
-    run()
+    question = Question("Is he married or not")
+    question.detect_type()
+    assert_equal(question.type, "open-ended")
